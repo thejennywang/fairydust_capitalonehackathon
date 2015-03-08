59957702c807c0a7791cb41a004ca27d6fb2aa58
Adam J. Mendoza <adammendoza@hotmail.com>
2015-03-08 12:55:20 -0700

    Clean up code

--- services/capitalone-levelmoney/lib/levelmoney.js
+++ services/capitalone-levelmoney/lib/levelmoney.js
@@ -39,71 +39,38 @@ module.exports = {
 
        var resultData = {"transactions":[], "totalsaved":0};
 
+      async.reduce(response.transactions, resultData, function(resultData, transaction, callback){
+          process.nextTick(function(){
 
-    async.reduce(response.transactions, resultData, function(resultData, transaction, callback){
-        process.nextTick(function(){
+          var newTransaction = {};
 
-        var newTransaction = {};
+          var date = new Date(transaction['transaction-time']);
+          var curr_date = date.getDate();
+          var curr_month = date.getMonth() + 1; //Months are zero based
+          var curr_year = date.getFullYear();
 
-        var date = new Date(transaction['transaction-time']);
-        var curr_date = date.getDate();
-        var curr_month = date.getMonth() + 1; //Months are zero based
-        var curr_year = date.getFullYear();
+          newTransaction['transaction-time'] =  curr_month + "." +  curr_date+ "." + curr_year;
 
-        newTransaction['transaction-time'] =  curr_month + "." +  curr_date+ "." + curr_year;
+          newTransaction['amount'] =  Math.abs(transaction['amount']);
 
-        newTransaction['amount'] =  Math.abs(transaction['amount']);
+          newTransaction['merchant'] = transaction['merchant'];
 
-        newTransaction['merchant'] = transaction['merchant'];
+          newTransaction['transaction-id'] = transaction['transaction-id'];
+          newTransaction['account-id'] = transaction['account-id'];
+          newTransaction['raw-merchant'] = transaction['raw-merchant'];
+          newTransaction['categorization'] = transaction['categorization'];
 
-        newTransaction['transaction-id'] = transaction['transaction-id'];
-        newTransaction['account-id'] = transaction['account-id'];
-        newTransaction['raw-merchant'] = transaction['raw-merchant']; 
-        newTransaction['categorization'] = transaction['categorization'];
+          newTransaction['saved'] =  Math.round(Math.abs(transaction['amount']) * 0.05);
 
-        newTransaction['saved'] =  Math.round(Math.abs(transaction['amount']) * 0.05);
+          resultData.totalsaved += newTransaction['saved'];
 
-        resultData.totalsaved += newTransaction['saved'];
+          resultData.transactions.push(newTransaction);
 
-        resultData.transactions.push(newTransaction);
-
-            callback(null, resultData );
-        });
-    }, function(err, result){
-        callback(resultData);
-    });
-
-
-
-      // response.transactions.forEach(function(v, i) {
-
-      //   var transaction = v;
-      //   var date = new Date(transaction['transaction-time']);
-      //   var curr_date = date.getDate();
-      //   var curr_month = date.getMonth() + 1; //Months are zero based
-      //   var curr_year = date.getFullYear();
-
-      //   transaction['transaction-time'] =  curr_month + "." +  curr_date+ "." + curr_year;
-
-
-      //   transaction['amount'] =  Math.abs(transaction['amount']);
-
-      //   transaction['saved'] =  Math.round(Math.abs(transaction['amount']) * 0.05);
-      //   totalsaved += transaction['saved'];
-      //  // console.log(transaction);
-       
-      //  json_transaction.push(transaction);
-
-      //  // obj['theTeam'].push({"teamId":"4","status":"pending"});
-      //  // jsonStr = JSON.stringify(obj);
-      //   //console.log(date);
-      // });
-
-      // callback(json_transaction);
-
-
-
-
+              callback(null, resultData );
+          });
+      }, function(err, result){
+          callback(resultData);
+      });
 
     });
   },
